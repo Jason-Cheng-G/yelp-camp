b1d862e7c8b6fe635baad1d20f6de1b09e4f5da9
Jason <goc558@mail.usask.ca>
2019-02-02 00:22:18 -0600

    set up layout,add nav and form, add basic styling

--- app.js
+++ app.js
@@ -1,35 +1,97 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+var campgrounds = [
+  {
+    name: "Salmon Creek",
+    image:
+      "https://images.unsplash.com/photo-1533873984035-25970ab07461?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1653&q=80"
+  },
+  {
+    name: "Granite Hill",
+    image:
+      "https://images.unsplash.com/photo-1515408320194-59643816c5b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+  },
+  {
+    name: "Mountain Goat's Rest",
+    image:
+      "https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
+  },
+  {
+    name: "Salmon Creek",
+    image:
+      "https://images.unsplash.com/photo-1533873984035-25970ab07461?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1653&q=80"
+  },
+  {
+    name: "Granite Hill",
+    image:
+      "https://images.unsplash.com/photo-1515408320194-59643816c5b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+  },
+  {
+    name: "Mountain Goat's Rest",
+    image:
+      "https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
+  },
+  {
+    name: "Salmon Creek",
+    image:
+      "https://images.unsplash.com/photo-1533873984035-25970ab07461?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1653&q=80"
+  },
+  {
+    name: "Granite Hill",
+    image:
+      "https://images.unsplash.com/photo-1515408320194-59643816c5b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+  },
+  {
+    name: "Mountain Goat's Rest",
+    image:
+      "https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
+  },
+  {
+    name: "Salmon Creek",
+    image:
+      "https://images.unsplash.com/photo-1533873984035-25970ab07461?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1653&q=80"
+  },
+  {
+    name: "Granite Hill",
+    image:
+      "https://images.unsplash.com/photo-1515408320194-59643816c5b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+  },
+  {
+    name: "Mountain Goat's Rest",
+    image:
+      "https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
+  }
+];
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.render("landing");
 });
 
-app.get("/campgrounds", function(req, res) {
-  var campgrounds = [
-    {
-      name: "Salmon Creek",
-      image:
-        "https://images.unsplash.com/photo-1533873984035-25970ab07461?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1653&q=80"
-    },
-    {
-      name: "Granite Hill",
-      image:
-        "https://images.unsplash.com/photo-1515408320194-59643816c5b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
-    },
-    {
-      name: "Mountain Goat's Rest",
-      image:
-        "https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
-    }
-  ];
+app.get("/campgrounds", function (req, res) {
   res.render("campgrounds", {
     campgrounds: campgrounds
   });
 });
 
-app.listen(process.env.PORT || 3000, process.env.IP, function() {
+app.post("/campgrounds", function (req, res) {
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = { name: name, image: image };
+  campgrounds.push(newCampground);
+  res.redirect("/campgrounds");
+  //get data from form and add to campgrounds array
+  //redirect back to campggrounds page
+});
+
+app.get("/campgrounds/new", function (req, res) {
+  res.render("new.ejs");
+});
+
+app.listen(process.env.PORT || 3000, process.env.IP, function () {
   console.log("YelpCampt Server Has Started!");
 });
