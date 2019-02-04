9f1fe1a64da93f7c60a9974de8e56bf57eb03ae6
Jason <goc558@mail.usask.ca>
2019-02-03 22:43:29 -0600

    add user-comment association

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ mongoose.connect("mongodb://127.0.0.1:27017/yelp_camp", {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-seedDB();
+//seed the campgrounds
+//seedDB();
 
 //PASSPORT CONFIGURATION
 app.use(
