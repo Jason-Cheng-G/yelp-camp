720205f12824396a9e20c7b12e7249c26d9ef70a
Jason <goc558@mail.usask.ca>
2019-02-04 15:59:04 -0600

    add Campground Edit and Update

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var User = require("./models/user");
 var seedDB = require("./seeds");
 var passport = require("passport");
 var LocalStrategy = require("passport-local");
+var methodOverride = require("method-override");
 var commentRoutes = require("./routes/comments");
 var campgroundRoutes = require("./routes/campgrounds");
 var indexRoutes = require("./routes/index");
@@ -19,6 +20,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/yelp_camp", {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
 //seed the campgrounds
 //seedDB();
 
